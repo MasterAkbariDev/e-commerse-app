5f4bb04fa481b65d07f0e93ae933c928fa4574d7
AliAkbar <masterakbaridev@gmail.com>
2024-08-12 18:54:52 +0330

    removed unused component and project is complete - waiting for needed changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Header />
         <Routes>
           <Route path='/' Component={Products} />
